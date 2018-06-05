db735b910fcec8dd1624f63c0657369652073c52
Natalia Castro <nat.alvz@gmail.com>
2018-06-05 21:25:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,11 @@ class Song
   end
   
   def self.new_by_filename(file_name)
+    song_name = file_name.split(" - ")[1]
+    song = self.new(song_name)
+    artist_name = file_name.split(" - ")[0]
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.artist.add_song(song)
+    song
   end
 end
